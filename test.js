71907cf8687e1390410e2716ae696653034d03c3
Maciej Walentynowicz <106319323+ValentinoMaciej@users.noreply.github.com>
2022-06-11 18:58:31 +0200

    zmiany na if false true

--- test.js
+++ test.js
@@ -1,8 +1,23 @@
-var youLikeMeat = true;
-var myNum = 10;
-if (myNum > 9){
+var youLikeMeat = false
+var myNum = 7;
+if (myNum == 10){
 
-    document.write("myNum is greater than 10...");
+    document.write("myNum is equal than 10...");
+
+}
+
+else{
+ document.write("myNum is not equal than 10...")
 
 
+}
+
+if(youLikeMeat){
+
+    document.write("Podaję menu mięsne...")
+
+}
+
+else{
+    document.write("Podaję menu wegańskie...")
 }
