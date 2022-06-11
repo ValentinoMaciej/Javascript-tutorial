49c6d2e7bc10604307b270007a86c9422aa0b295
Maciej Walentynowicz <106319323+ValentinoMaciej@users.noreply.github.com>
2022-06-11 19:33:37 +0200

    while loops z komentarzami

--- test.js
+++ test.js
@@ -1,12 +1,14 @@
-var myAge = 12;
+// while loop - podczas gdy warunek jest spełniony (podobne do if)
+var age = 8;
 
-// || - oznacza ALBO, && = oznacza ORAZ(TAKŻE)
+while (age < 10){
 
-if (myAge < 18 || myAge > 30 || myAge == 25){
+console.log("Twój wiek to mniej niż 10 lat");
+age++;
+//dodanie age++ aby kod się nie zapętlił - będzie dodawac po 1 roku
 
-document.write("Nie możesz wejść")
 
-} else{
 
-    document.write("Możesz wejść")
 }
+
+document.write("Twój wiek to więcej niż 10 lat");
