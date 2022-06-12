2d8b94f5b240bf4afcaa51f66c3102322a371467
Maciej Walentynowicz <106319323+ValentinoMaciej@users.noreply.github.com>
2022-06-12 20:26:23 +0200

    funkcje, return, var etc.

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
-//praktyczne zastosowanie pętli
+function getAverage (a,b,c,d,e){
 
-var links = document.getElementsByTagName("a");
+    var average = (a + b + c + d + e) / 2;
+    console.log(average);
+    return average;
 
-for(i = 0; i < links.length; i++){
-
-links[i].className = "link-" + i;
 
 }
-/*
-Przypisanie do danego przykładu pętli mówiącej o tym
-że następuje numeracja elementu znajsującego się w 
-<a> w tym przypadku oznumerowanie linków
-*/
+
+var myResult = getAverage(7,12,23,9,3);
+console.log("The average is " + myResult)
+
+
+/* jeśli funkcja zawiera dane (a,b) wtedy funkcja musi być
+również wywołana z danymi w nawiasie inaczej będzie błąd,
+jeśli są 2 wartości to wywołujemy 2, inaczej sa ignorowane
+powyżej tej liczby*/
+//return wywołuje funkcję jak w przykładzie
