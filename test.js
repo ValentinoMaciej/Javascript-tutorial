afd91cf2b460e827643ac88d5f418c4c3f0b6fa1
Maciej Walentynowicz <106319323+ValentinoMaciej@users.noreply.github.com>
2022-06-13 22:07:57 +0200

    wstęp do object -okreslanie funkcji/metod obiektów

--- test.js
+++ test.js
@@ -1,20 +1,6 @@
 /*
-myArray można okreslić np.
-myArray[0] = 1
-myArray[1] = "hello"
-myArray[2] = true
-myArray[3] = false
 
-LUB
-
-myArray[1,"hello",true,false]
-
-i jest to to samo co zapisek wyżej
-
-można też posortować Array lub odwrócić czy sprawdzić długość np.
-
-myArray.length - dlugosc czyli 4 pozycje
-myArray.sort - od najmniejszej do najwiekszej
-myArray.reverse - na odwrót
+wstęp do obcject
+okreslanie funkcji i metod dla obiektów
 
 */
