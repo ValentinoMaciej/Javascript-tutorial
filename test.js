2616341a78232bfd770305af34980bace2766570
Maciej Walentynowicz <106319323+ValentinoMaciej@users.noreply.github.com>
2022-06-14 12:28:06 +0200

    THIS keyword z poprawnymi danymi

--- test.js
+++ test.js
@@ -1,16 +1,3 @@
-/*
-
-Kreowanie nowych obiektów
-
-
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Shaun";
-myCar.drive = function(){
-console.log("Teraz prowadzę");};
-
-Okreslanie funkcji obiektu, ale można też w inny sposób
-
 var myCar2 = {
 
     maxSpeed: 70,
@@ -18,22 +5,16 @@ var myCar2 = {
     drive: function(speed, time){
         console.log(speed * time);
 
-    }
-    logDriver: function(){
+    },
+    logDriver: function() {
         console.log("Driver name is " + this.driver);
-// THIS wywołuje obiekt pod ktory jest podpięty
-THIS na początku kodu oznaczałoby że w logu
-pojawią się wszystkie wartości
-Natomiast THIS w powyższym przykładzie odwołuje się
-do wartości driver z myCar2
     }
 
-    Jest to samo co u góry ale w innym zapisie
+    };
 
-};
+
 
 myCar2.logDriver();
 console.log(myCar2.maxSpeed);
 myCar2.drive(50,3);
 
-*/
