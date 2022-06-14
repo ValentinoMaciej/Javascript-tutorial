f02f81cf20be21199a2c5b024a5b871a5e880459
Maciej Walentynowicz <106319323+ValentinoMaciej@users.noreply.github.com>
2022-06-14 11:37:08 +0200

    kreowanie obiektów nowych i określanie funkcji

--- test.js
+++ test.js
@@ -1,6 +1,30 @@
 /*
 
-wstęp do obcject
-okreslanie funkcji i metod dla obiektów
+Kreowanie nowych obiektów
+
+
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Shaun";
+myCar.drive = function(){
+console.log("Teraz prowadzę");};
+
+Okreslanie funkcji obiektu, ale można też w inny sposób
+
+var myCar2 = {
+
+    maxSpeed: 70,
+    driver: "Net Ninja",
+    drive: function(speed, time){
+        console.log(speed * time);
+
+    }
+
+    Jest to samo co u góry ale w innym zapisie
+
+};
+
+console.log(myCar2.maxSpeed);
+myCar2.drive(50,3);
 
 */
