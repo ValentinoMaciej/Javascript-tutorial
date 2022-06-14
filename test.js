582d0f10dd09a2885228893c490326ed181535c2
Maciej Walentynowicz <106319323+ValentinoMaciej@users.noreply.github.com>
2022-06-14 13:06:00 +0200

    funkcja construction

--- test.js
+++ test.js
@@ -1,20 +1,27 @@
-var myCar2 = {
+var Car = function(maxSpeed, driver){
 
-    maxSpeed: 70,
-    driver: "Net Ninja",
-    drive: function(speed, time){
-        console.log(speed * time);
+this.maxSpeed = maxSpeed;
+this.driver = driver;
+this.drive = function(speed,time){
+    console.log(speed * time);
+};
+this.logDriver = function(){
+    console.log("driver name is " + this.driver);
 
-    },
-    logDriver: function() {
-        console.log("Driver name is " + this.driver);
-    }
-
-    };
+};
 
 
+}
 
-myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50,3);
+var myCar = new Car(70, "Człowiek baton");
+var myCar2 = new Car(15, "Człowiek");
+var myCar3 = new Car(40, "Baton");
+var myCar4 = new Car(34, "Pies baton");
 
+myCar.drive(10,2);
+myCar3.logDriver();
+
+/* funkcja construction daje mozliwosc utworzenia
+jednej fukncji w ramach której mozemy dodawac
+zmienne bez koniecznosci tworzenia kolejnych
+takich samych lub podobnych funkcji w nieskonczonsc */
