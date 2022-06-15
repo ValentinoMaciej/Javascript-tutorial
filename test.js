ca2b0220a86b78be9426811d3e68aadb9b5a0600
Maciej Walentynowicz <106319323+ValentinoMaciej@users.noreply.github.com>
2022-06-15 21:19:20 +0200

    JS form validation 44 lekcja the net ninja

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
 /*
-JS For Aktywowanie Form elementów - formularzy
-
-document.forms.nazwa-form;
-
-nazwa-form
-nazwa-form.name
-nazwa-form.value
-
-
-
+JS For Rofm Validation
+najprostsze sprawdzenie co została wpisane do formularza
 
 
 */
+
+var myForm = document.forms.myForm;
+var message = document.getElementById("message");
+
+myForm.onsubmit = function () {
+  if (myForm.name.value == "") {
+    message.innerHTML = "please enter name";
+    return false;
+  } else {
+    message.innerHTML = "";
+    return true;
+  }
+};
