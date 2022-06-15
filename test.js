326d7053fc9f8b96a5edd726f4d2cc48eb01db87
Maciej Walentynowicz <106319323+ValentinoMaciej@users.noreply.github.com>
2022-06-15 21:32:14 +0200

    JS libriaries jQUERY

--- test.js
+++ test.js
@@ -1,19 +1,9 @@
 /*
-JS For Rofm Validation
-najprostsze sprawdzenie co została wpisane do formularza
+JS libraries
+czyli biblioteka skryptów javascript, którą można używać
+np. wideo, animacje, generalny kod, formularze
+najpopularniejszy jest jQUERY
+jQUERY.com
 
 
 */
-
-var myForm = document.forms.myForm;
-var message = document.getElementById("message");
-
-myForm.onsubmit = function () {
-  if (myForm.name.value == "") {
-    message.innerHTML = "please enter name";
-    return false;
-  } else {
-    message.innerHTML = "";
-    return true;
-  }
-};
