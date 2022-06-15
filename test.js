9c759d47573ed7356a6a330c9bf5765721946dcc
Maciej Walentynowicz <106319323+ValentinoMaciej@users.noreply.github.com>
2022-06-15 21:14:57 +0200

    JS forms 43 the net ninja lekcja

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
 /*
-JS timer
-czyli ustalanie czasówki po jakiej np. ma się pojawić obiekt,
-ma być wywołana funkcja
+JS For Aktywowanie Form elementów - formularzy
 
-setTimeout(jaka funkcja, milisekundy do wyswietlenia);
+document.forms.nazwa-form;
 
-setInterval(jaka funkcja np. zmiana kolory, co ile)
-clearInterval - czyli zatrzymanie interwału poprzez np.
-fukncje onclick czyli kliknięcie na dany obiekt
+nazwa-form
+nazwa-form.name
+nazwa-form.value
+
+
+
+
+
 */
